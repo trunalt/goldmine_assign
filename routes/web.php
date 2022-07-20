a28de620b99808049d461ad8c366ed319955cdd9
trunalt <trunaltamore@gmail.com>
2022-07-20 12:53:58 +0530

    login route change

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return redirect('/login');
+});
 
 Route::get('{any}', function () {
     return view('layouts.app');
